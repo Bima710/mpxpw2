115dac39fe20fddaea4a62c45e44d8a9a03f1834
Bima710 <moch.bima@student.umn.ac.id>
2024-09-18 10:55:18 +0700

    update

--- week2/App.js
+++ week2/App.js
@@ -28,20 +28,49 @@
 //   );
 // }
 
-import React from 'react';
-import { View, Text, Image, ScrollView } from 'react-native';
-import data from './data.json';
+// import React from 'react';
+// import { View, Text, Image, ScrollView } from 'react-native';
+// import data from './data.json';
+
+// export default function App() {
+//   return (
+//     <ScrollView>
+//       {data.map((person, index) => (
+//         <View key={index} style={{ alignItems: 'center' }}>
+//           <Image source={{ uri: person.photo_url }} style={{ width: 100, height: 100 }} />
+//           <Text>{person.name}</Text>
+//           <Text>{person.email}</Text>
+//         </View>
+//       ))}
+//     </ScrollView>
+//   );
+// };
+
+import { Text, View, Image, ScrollView} from "react-native";
+import userData from "./data.json";
+import styles from "./App.styles";
 
 export default function App() {
   return (
     <ScrollView>
-      {data.map((person, index) => (
-        <View key={index} style={{ alignItems: 'center' }}>
-          <Image source={{ uri: person.photo_url }} style={{ width: 100, height: 100 }} />
-          <Text>{person.name}</Text>
-          <Text>{person.email}</Text>
-        </View>
-      ))}
-    </ScrollView>
+      {userData.map((users) => {
+        return (
+          <View style={StyleSheet.container} key={users.name}>
+            <View style={StyleSheet.card}>
+              <Image
+                source={{
+                  uri: users.photo_url,
+                }}
+                style={StyleSheet.avatar}
+                />
+                <View style={StyleSheet.boldText}>
+                  <Text style={StyleSheet.boldText}>{users.name}</Text>
+                  <Text>{users.email}</Text>
+                  </View>
+                </View>
+            </View>
+        );
+      })}
+      </ScrollView>
   );
-};
+}
